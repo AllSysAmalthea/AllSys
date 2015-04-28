4df322bc9819eb7937ff336c189612e3e36ffd82
OgShun <121220052@smail.nju.edu.cn>
2015-04-28 20:23:38 +0800

    app
    界面

---
+++ APP/AppUI code/代码/Allsys/src/com/example/allsys/RegularActivity.java
@@ -0,0 +1,37 @@
+package com.example.allsys;
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class RegularActivity extends Activity {
+
+	
+	
+    @SuppressLint("NewApi")
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);  
+        setContentView(R.layout.activity_regular); 
+                       
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.log_in, menu);
+        return true;
+    }
+    
+}
